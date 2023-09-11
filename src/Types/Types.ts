a2efc2beb54a6ad7278ab74eea70fa4391ba5d22
Het <hetdesai068@gmail.com>
2023-09-11 12:20:39 +0530

    Restructured types

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -2,18 +2,25 @@ type currentItemDataType = {
     id: string, title: string
 }
 
+type todoItemType = {title: string, id: string, isCompleted: boolean}
+
+type todoDataSaveHandlerFuncType = (data: todoItemType[]) => void
+
 type addItemInTodoListParamType = {
+    isNewItem: boolean, title: string, existingUniqueId: string | null, todoData: todoItemType[], todoDataSaveHandler: todoDataSaveHandlerFuncType, setIsAddingItem: (value: boolean)=>void, setIsEditingItem: (value: boolean)=>void
+}
+
+type addItemInTodoListHandlerParamType = {
     isNewItem: boolean, title: string, existingUniqueId: string | null
 }
 
-type todoItemType = {title: string, id: string, isCompleted: boolean}
-
-type addItemInTodoListFuncType = ({isNewItem, title, existingUniqueId}:addItemInTodoListParamType) => void
+type addItemInTodoListFuncType = ({isNewItem, title, existingUniqueId, todoData, todoDataSaveHandler, setIsAddingItem, setIsEditingItem}:addItemInTodoListParamType) => void
+type addItemInTodoListHandlerFuncType = ({isNewItem, title, existingUniqueId}:addItemInTodoListHandlerParamType) => void
 
 type editItemFuncType = (id:string, currentTitle: string)=>void
 
 type deleteItemHandlerFuncType = (id: string)=>void
 
-type toggleItemMarkedCompletedFuncType = (id:string)=>void;
+type toggleItemMarkedCompletedHandlerFuncType = (id:string)=>void;
 
-export type {currentItemDataType, addItemInTodoListParamType, addItemInTodoListFuncType, todoItemType, editItemFuncType, deleteItemHandlerFuncType, toggleItemMarkedCompletedFuncType}
+export type {currentItemDataType, addItemInTodoListParamType, addItemInTodoListFuncType, todoItemType, editItemFuncType, deleteItemHandlerFuncType, toggleItemMarkedCompletedHandlerFuncType, todoDataSaveHandlerFuncType, addItemInTodoListHandlerFuncType}
